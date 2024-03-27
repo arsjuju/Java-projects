6fddc1e96d21dc96e09adae362903faced4718d8
Juliana <160780965+arsjuju@users.noreply.github.com>
2024-03-27 15:19:56 -0300

    Add files via upload

---
+++ ExAbstract/src/exabstract/Proprietario.java
@@ -0,0 +1,52 @@
+package exabstract;
+
+/**
+ *
+ * @author 357510
+ */
+public class Proprietario {
+
+    public String cpf;
+    public String nome;
+    public Endereco endereco;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Proprietario(String cpf, String nome, Endereco endereco) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+
+    @Override
+    public String toString() {
+        return "\n-----------------------------------"
+                + "\nDADOS DO PROPRIETÁRIO"
+                + "\n-----------------------------------"
+                + "\nCpf: " + getCpf()
+                + "\nNome: " + getNome()
+                + "\nEndereço: " + getEndereco();
+    }
+}
